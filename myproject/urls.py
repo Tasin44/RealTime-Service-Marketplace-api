7adfdcd18c3dc57dcb2965c68867fa68e8764402
root <root@srv1455426.hstgr.cloud>
2026-05-16 06:52:16 +0000

    update base url .html file

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,26 +1,12 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='welcome.html'), name='home'),
     path('admin/', admin.site.urls),
     path('auth/',include('authapp.urls')),
     path('provider/', include('serviceproviderapp.urls')),
@@ -29,6 +15,7 @@ urlpatterns = [
     path('message/', include('messageapp.urls')),
     path('notification/', include('notificationapp.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
